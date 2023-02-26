61bd97a26be88da410138b7d3fde611a6260e088
khiem <khiemcan@gmail.com>
2023-02-27 01:15:25 +0700

    remove name property of user

--- controllers/users.js
+++ controllers/users.js
@@ -16,19 +16,17 @@ usersRouter.get('/', async (request, response) => {
 })
 
 usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+  const { username, password } = request.body
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
   const user = new User({
     username,
-    name,
     passwordHash,
   })
 
   const savedUser = await user.save()
-
   response.status(201).json(savedUser)
 })
 
