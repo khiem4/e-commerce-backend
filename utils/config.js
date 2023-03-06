8e3bd9965ed909a44c4084d8dbb10d15a30397fa
khiem <khiemcan@gmail.com>
2023-03-07 01:17:20 +0700

    build:ui

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
-require('dotenv').config()
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+console.log('MONGODB_URI:', MONGODB_URI)
 
 module.exports = {
   MONGODB_URI,
