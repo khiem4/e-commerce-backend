554fb9cd86802e3634641afa506bca59825f1f22
khiem <khiemcan@gmail.com>
2023-03-06 22:48:42 +0700

    added build from front-end

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(middleware.requestLogger)
 
 app.use('/api/cart', cartRouter)
