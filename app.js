d0d4fd24324adfee9276fcb903238a0190e6eb8b
khiem <khiemcan@gmail.com>
2023-03-07 03:18:33 +0700

    removed mongoose connect obj

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ mongoose.set('strictQuery', false)
 console.log('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI ,{
-  useUnifiedTopology:true,
-  useNewUrlParser: true,
-  useCreateIndex: true
 })
   .then(() => {
     console.log('connected to MongoDB')
